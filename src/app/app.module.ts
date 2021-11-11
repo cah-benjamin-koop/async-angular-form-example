460e2b6bd996f39ba4cbf5cb5dfb8590b16fdc3d
Ben Koop <benjamin.koop@cardinalhealth.com>
2021-11-11 09:27:20 -0500

    Added mroe displayed information

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { InputFormComponent } from './input-form/input-form.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgPipesModule } from 'ngx-pipes';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    NgPipesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
